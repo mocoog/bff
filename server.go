8326a2163a593972e1f089537ec55737628a83fb
Hirochon <heacet43@gmail.com>
2021-12-12 11:26:55 +0000

    update: refactoring

--- server.go
+++ server.go
@@ -4,42 +4,23 @@ import (
 	"flag"
 	"log"
 
-	"github.com/go-openapi/loads"
-	"github.com/go-openapi/runtime/middleware"
-	"github.com/mocoog/bff/interface/gen/restapiv1"
-	"github.com/mocoog/bff/interface/gen/restapiv1/operations"
-	"github.com/mocoog/bff/interface/gen/restapiv1/operations/basis"
+	"github.com/mocoog/bff/interface/provider"
+	"github.com/mocoog/bff/utils"
 )
 
 var portFlag = flag.Int("port", 8080, "Port to run this service on")
 
-func initializeAPI() (*operations.MocoogBffServerAPI, error) {
-	swaggerSpec, err := loads.Analyzed(restapiv1.SwaggerJSON, "")
-	if err != nil {
-		return nil, err
-	}
-
-	api := operations.NewMocoogBffServerAPI(swaggerSpec)
-	return api, nil
-}
-
 func main() {
-	api, err := initializeAPI()
+	server, err := provider.InitializeAPIServer()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln(utils.CustomErrorLog(err))
 	}
-	server := restapiv1.NewServer(api)
 	defer server.Shutdown()
 
 	flag.Parse()
 	server.Port = *portFlag
 
-	api.BasisGetHealthHandler = basis.GetHealthHandlerFunc(
-		func(params basis.GetHealthParams) middleware.Responder {
-			return basis.NewGetHealthOK()
-		})
-
 	if err := server.Serve(); err != nil {
-		log.Fatalln(err)
+		log.Fatalln(utils.CustomErrorLog(err))
 	}
 }
