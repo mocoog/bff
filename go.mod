afe2a74a7b1622151090e06af6e06557904aa82a
Hirochon <heacet43@gmail.com>
2021-12-13 17:50:57 +0000

    wip:

--- go.mod
+++ go.mod
@@ -10,7 +10,10 @@ require (
 	github.com/go-openapi/strfmt v0.21.0
 	github.com/go-openapi/swag v0.19.15
 	github.com/jessevdk/go-flags v1.5.0
+	github.com/mocoog/grpc-go-packages v0.0.0-20211212154713-1d303f8f5b46
+	github.com/pkg/errors v0.9.1
 	golang.org/x/net v0.0.0-20211111083644-e5c967477495
+	google.golang.org/grpc v1.42.0
 )
 
 require (
@@ -23,6 +26,7 @@ require (
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/validate v0.20.3 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
@@ -30,5 +34,7 @@ require (
 	go.mongodb.org/mongo-driver v1.7.3 // indirect
 	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- interface/provider/initialize_api_server.go
+++ interface/provider/initialize_api_server.go
@@ -1,14 +1,64 @@
 package provider
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/go-openapi/loads"
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/mocoog/bff/interface/gen/restapiv1"
 	"github.com/mocoog/bff/interface/gen/restapiv1/operations"
 	"github.com/mocoog/bff/interface/gen/restapiv1/operations/basis"
+	gPB "github.com/mocoog/grpc-go-packages/gateway/proto"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
+
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second,
+	Timeout:             5 * time.Second,
+	PermitWithoutStream: true,
+}
+
+func healthCheckImp(params basis.GetHealthParams) middleware.Responder {
+	address := "172.23.0.1:9881"
+	conn, err := grpc.Dial(
+		address,
+		grpc.WithInsecure(),
+		// grpc.WithBlock(),
+		grpc.WithKeepaliveParams(kacp),
+	)
+	if err != nil {
+		panic(errors.Wrap(err, "コネクションエラー"))
+	}
+	defer conn.Close()
+
+	client := gPB.NewGatewayServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Second,
+	)
+	defer cancel()
+
+	healthCheckRequest := gPB.HealthCheckRequest{
+		Status: 200,
+	}
+
+	res, err := client.HealthCheck(ctx, &healthCheckRequest)
+
+	if err != nil {
+		log.Println(errors.Wrap(err, "受取り失敗"))
+	} else {
+		log.Printf("サーバからの受け取り %d", res.GetStatus())
+	}
+
+	// return request(client, a, b)
+	return basis.NewGetHealthOK()
+}
 
 func InitializeAPIServer() (*restapiv1.Server, error) {
 	swaggerSpec, err := loads.Analyzed(restapiv1.SwaggerJSON, "")
@@ -17,9 +67,6 @@ func InitializeAPIServer() (*restapiv1.Server, error) {
 	}
 
 	api := operations.NewMocoogBffServerAPI(swaggerSpec)
-	api.BasisGetHealthHandler = basis.GetHealthHandlerFunc(
-		func(params basis.GetHealthParams) middleware.Responder {
-			return basis.NewGetHealthOK()
-		})
+	api.BasisGetHealthHandler = basis.GetHealthHandlerFunc(healthCheckImp)
 	return restapiv1.NewServer(api), nil
 }
